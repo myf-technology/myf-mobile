78869521c363fd8ef5459f5cfa363c8209e0c65e
danielanthonyl <danskyjs@icloud.com>
2022-08-21 00:54:34 -0300

    feature: add ref

--- src/components/Input/_components/Input/index.tsx
+++ src/components/Input/_components/Input/index.tsx
@@ -1,9 +1,5 @@
-import { useState, useEffect } from 'react';
-import {
-  View,
-  NativeSyntheticEvent,
-  TextInputFocusEventData,
-} from 'react-native';
+import { useState, useEffect, forwardRef, Ref } from 'react';
+import { TextInput, View } from 'react-native';
 import MaskInput from 'react-native-mask-input';
 
 import { inputStyles, styles } from './styles';
@@ -14,75 +10,65 @@ import { Text, Spacer } from '../../../';
 import { IInputProps } from './types';
 import { Icon } from 'react-native-eva-icons';
 
-export const Input = ({
-  mask,
-  errorMessage,
-  passwordMode,
-  suffixIcon,
-  ...rest
-}: IInputProps) => {
-  const [eyeIconVisibility, setEyeIconVisibility] = useState(false);
+export const Input = forwardRef(
+  (
+    { mask, errorMessage, passwordMode, suffixIcon, ...rest }: IInputProps,
+    ref: Ref<TextInput>,
+  ) => {
+    const [eyeIconVisibility, setEyeIconVisibility] = useState(false);
 
-  const focusHandler = (e: NativeSyntheticEvent<TextInputFocusEventData>) => {
-    rest?.onFocus && rest?.onFocus(e);
-  };
+    useEffect(() => {
+      passwordMode && setEyeIconVisibility(true);
+    }, [passwordMode, setEyeIconVisibility]);
 
-  const blurHandler = (e: NativeSyntheticEvent<TextInputFocusEventData>) => {
-    rest?.onBlur && rest?.onBlur(e);
-  };
+    const toggleVisibility = () => {
+      setEyeIconVisibility(!eyeIconVisibility);
+    };
 
-  useEffect(() => {
-    passwordMode && setEyeIconVisibility(true);
-  }, [passwordMode, setEyeIconVisibility]);
-
-  const toggleVisibility = () => {
-    setEyeIconVisibility(!eyeIconVisibility);
-  };
-
-  return (
-    <View>
-      <View style={styles.inputContainer}>
-        <MaskInput
-          style={inputStyles()}
-          numberOfLines={1}
-          placeholderTextColor={Colors.white}
-          secureTextEntry={eyeIconVisibility}
-          mask={mask}
-          value=""
-          autoFocus
-          {...rest}
-          onFocus={focusHandler}
-          onBlur={blurHandler}
-        />
-        {suffixIcon && suffixIcon}
-        {passwordMode &&
-          (eyeIconVisibility ? (
-            <Icon
-              fill="white"
-              onPress={toggleVisibility}
-              name="eye"
-              width={width(10)}
-              height={width(6)}
-            />
-          ) : (
-            <Icon
-              width={width(10)}
-              height={width(6)}
-              fill="white"
-              name="eye-off"
-              onPress={toggleVisibility}
-              animation="zoom"
-            />
-          ))}
+    return (
+      <View>
+        <View style={styles.inputContainer}>
+          <MaskInput
+            ref={ref}
+            style={inputStyles()}
+            value=""
+            numberOfLines={1}
+            placeholderTextColor={Colors.white}
+            secureTextEntry={eyeIconVisibility}
+            mask={mask}
+            autoFocus
+            {...rest}
+          />
+          {suffixIcon && suffixIcon}
+          {passwordMode &&
+            (eyeIconVisibility ? (
+              <Icon
+                fill="white"
+                onPress={toggleVisibility}
+                name="eye"
+                width={width(10)}
+                height={width(6)}
+              />
+            ) : (
+              <Icon
+                width={width(10)}
+                height={width(6)}
+                fill="white"
+                name="eye-off"
+                onPress={toggleVisibility}
+                animation="zoom"
+              />
+            ))}
+        </View>
+        {errorMessage ? (
+          <>
+            <Spacer amount={2} />
+            <Text color="red" typography="headline">
+              {errorMessage}
+            </Text>
+          </>
+        ) : null}
       </View>
-      {errorMessage ? (
-        <>
-          <Spacer amount={2} />
-          <Text color="red" typography="headline">
-            {errorMessage}
-          </Text>
-        </>
-      ) : null}
-    </View>
-  );
-};
+    );
+  },
+);
