9d7a0340073d8c9483059b1abfd1def9836be01b
danielanthonyl <danskyjs@icloud.com>
2022-08-21 00:22:40 -0300

    feature: updated commit

--- src/components/Input/_components/Input/index.tsx
+++ src/components/Input/_components/Input/index.tsx
@@ -1,10 +1,8 @@
 import { useState, useEffect } from 'react';
 import {
   View,
-  TouchableOpacity,
   NativeSyntheticEvent,
   TextInputFocusEventData,
-  ActivityIndicator,
 } from 'react-native';
 import MaskInput from 'react-native-mask-input';
 
@@ -12,97 +10,79 @@ import { inputStyles, styles } from './styles';
 
 import { Colors } from '../../../../constants';
 import { width } from '../../../../constants/Responsive';
-import { Icon, Text, Spacer } from '../../../';
+import { Text, Spacer } from '../../../';
 import { IInputProps } from './types';
+import { Icon } from 'react-native-eva-icons';
 
 export const Input = ({
   mask,
-  textColor = 'white',
-  errorMessage = '',
-  placeholderAlign = 'left',
-  passwordMode = true,
-  toggleVisibility,
-  arrowGo,
-  sufixIcon = 'ArrowForward',
-  loading,
-  onArrowPress,
-  containerTestID,
-  arrowPosition,
+  errorMessage,
+  passwordMode,
+  suffixIcon,
   ...rest
 }: IInputProps) => {
   const [eyeIconVisibility, setEyeIconVisibility] = useState(false);
-  const [textcolors, setTextColors] = useState(Colors.black);
-
-  const isErrorState = textcolors === Colors.red;
 
   const focusHandler = (e: NativeSyntheticEvent<TextInputFocusEventData>) => {
     rest?.onFocus && rest?.onFocus(e);
-    setTextColors(Colors.black);
   };
 
   const blurHandler = (e: NativeSyntheticEvent<TextInputFocusEventData>) => {
     rest?.onBlur && rest?.onBlur(e);
-
-    const shouldSetError = errorMessage !== '';
-
-    shouldSetError && setTextColors(Colors.red);
   };
 
   useEffect(() => {
     passwordMode && setEyeIconVisibility(true);
   }, [passwordMode, setEyeIconVisibility]);
 
-  useEffect(() => {
-    const shouldSetError = errorMessage !== '';
-
-    shouldSetError && setTextColors(Colors.red);
-    !shouldSetError && setTextColors(Colors.white);
-  }, [errorMessage]);
+  const toggleVisibility = () => {
+    setEyeIconVisibility(!eyeIconVisibility);
+  };
 
   return (
-    <View testID={containerTestID}>
-      <MaskInput
-        style={inputStyles({
-          placeholderAlign,
-          textColor,
-        })}
-        numberOfLines={1}
-        placeholderTextColor={Colors.white}
-        testID="textinput-input-component"
-        secureTextEntry={passwordMode}
-        mask={mask}
-        value=""
-        {...rest}
-        onFocus={focusHandler}
-        onBlur={blurHandler}
-      />
-      {arrowGo ? (
-        <TouchableOpacity
-          onPress={() => onArrowPress && onArrowPress()}
-          style={{ position: 'absolute', top: arrowPosition, left: width(80) }}>
-          {loading ? (
-            <ActivityIndicator
-              color={Colors.yellow}
-              style={{ right: width(3), bottom: width(1) }}
+    <View>
+      <View style={styles.inputContainer}>
+        <MaskInput
+          style={inputStyles()}
+          numberOfLines={1}
+          placeholderTextColor={Colors.white}
+          secureTextEntry={eyeIconVisibility}
+          mask={mask}
+          value=""
+          autoFocus
+          {...rest}
+          onFocus={focusHandler}
+          onBlur={blurHandler}
+        />
+        {suffixIcon && suffixIcon}
+        {passwordMode &&
+          (eyeIconVisibility ? (
+            <Icon
+              fill="white"
+              onPress={toggleVisibility}
+              name="eye"
+              width={width(10)}
+              height={width(6)}
             />
           ) : (
-            <Icon fill="yellow" name={sufixIcon} />
-          )}
-        </TouchableOpacity>
+            <Icon
+              width={width(10)}
+              height={width(6)}
+              fill="white"
+              name="eye-off"
+              onPress={toggleVisibility}
+              animation="zoom"
+            />
+          ))}
+      </View>
+      {errorMessage ? (
+        <>
+          <Spacer amount={2} />
+          <Text color="red" typography="headline">
+            {errorMessage}
+          </Text>
+        </>
       ) : null}
-      {eyeIconVisibility && (
-        <TouchableOpacity onPress={toggleVisibility} style={styles.eyeWrapper}>
-          {passwordMode ? (
-            <Icon fill="yellow2" name="ArrowForward" />
-          ) : (
-            <Icon fill="yellow2" name="ArrowForward" />
-          )}
-        </TouchableOpacity>
-      )}
-      <Spacer amount={2} />
-      <Text color="white" textAlign="left" typography="callout">
-        {isErrorState && errorMessage}
-      </Text>
     </View>
   );
 };
