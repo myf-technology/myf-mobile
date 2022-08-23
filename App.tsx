68edb0067d0d456b738747a01b150e8ebfe6b6cd
Djonatan Abreu <djonatan@Djonatans-MacBook-Air.local>
2022-08-22 22:02:51 -0300

    feature: add react redux toolkits

--- App.tsx
+++ App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import {View} from 'react-native';
 import {Login} from './src/views/Login';
+import {Provider} from 'react-redux';
+import {store} from './src/store/store';
 
 export default () => {
   return (
-    <View
-      style={{
-        backgroundColor: 'black',
-        flex: 1,
-      }}>
-      <Login />
-    </View>
+    <Provider store={store}>
+      <View
+        style={{
+          backgroundColor: 'black',
+          flex: 1,
+        }}>
+        <Login />
+      </View>
+    </Provider>
   );
 };
