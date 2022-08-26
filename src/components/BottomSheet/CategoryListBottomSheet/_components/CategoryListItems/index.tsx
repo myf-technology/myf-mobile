ec70c8b3754b17765417338790f7bee164f19365
otherRick <drckbr@gmail.com>
2022-08-26 19:01:18 -0300

    feat: remove load and set empty view

--- src/components/BottomSheet/CategoryListBottomSheet/_components/CategoryListItems/index.tsx
+++ src/components/BottomSheet/CategoryListBottomSheet/_components/CategoryListItems/index.tsx
@@ -1,18 +1,32 @@
+import { useRef } from 'react';
 import { View } from 'react-native';
 import { useSelector } from 'react-redux';
 import { IStore } from '../../../../../store/types';
+import { CreateCategorySheet } from '../../../../CreateCategorySheet';
+import { Text } from '../../../../Text';
 import { CategoryListItem } from '../CategoryListItem';
+import { EmptyCategoryList } from '../EmptyCategoryList';
 import styles from './styles';
 import { CategoryListLoading } from './_components/CategoryListLoading';
 
 export const CategoryListItems = () => {
   const {
-    controls: { status },
+    controls: { status, balanceType },
     list,
   } = useSelector(({ bottomSheet }: IStore) => bottomSheet);
 
+  if (list.length === 0) {
+    const type = balanceType === 'INCOME' ? '"Ganhos"' : '"Despesas"';
+    const message = `Não há categoria do tipo \n${type}\nclick no icone abaixo para criar uma nova categoria`;
+    return <EmptyCategoryList message={message} />;
+  }
+
   if (status === 'loading') {
-    return <CategoryListLoading />;
+    return (
+      <View>
+        <CategoryListLoading />;
+      </View>
+    );
   }
 
   return (
