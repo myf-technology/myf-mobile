160c706333dd1529f8c53e7251e4bcd36f9bf38d
danielanthonyl <danskyjs@icloud.com>
2022-08-28 01:30:08 -0300

    ✨ feature: add ability to create new category and add new category create button

--- src/components/BottomSheet/CategoryListBottomSheet/_components/CategoryListItems/_components/CategoryListEmpty/index.tsx
+++ src/components/BottomSheet/CategoryListBottomSheet/_components/CategoryListItems/_components/CategoryListEmpty/index.tsx
@@ -1,22 +1,26 @@
 import { View } from 'react-native';
 import { Text } from '../../../../../../Text';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { IStore } from '../../../../../../../store/types';
 
 import styles from './styles';
 import { Button } from '../../../../../../Button';
 import { Spacer } from '../../../../../../Spacer';
+import { manageCategoryBottomSheetControl } from '../../../../../ManageCategoryBottomSheet';
 
 export const CategoryListEmpty = () => {
   const {
     controls: { balanceType },
   } = useSelector(({ bottomSheet }: IStore) => bottomSheet);
+  const dispatch = useDispatch();
 
   const type = balanceType === 'INCOME' ? '"Ganhos"' : '"Despesas"';
   const message = `Não temos categorias do tipo ${type}`;
 
-  const onPress = () => {};
+  const onPress = () => {
+    dispatch(manageCategoryBottomSheetControl({ visible: true }));
+  };
 
   return (
     <View style={styles.textContainer}>
--- src/components/BottomSheet/CategoryListBottomSheet/_components/CategoryListItems/_components/index.ts
+++ src/components/BottomSheet/CategoryListBottomSheet/_components/CategoryListItems/_components/index.ts
@@ -1,3 +1,4 @@
 export { CategoryListEmpty } from './CategoryListEmpty';
 export { CreateCategoryError } from './CategoryListError';
 export { CategoryListLoading } from './CategoryListLoading';
+export { CreateNewCategoryItem } from './CreateNewCategoryItem';
