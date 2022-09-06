4ebdfb9f3f838e21732151c789adb09ab0273887
danielanthonyl <danskyjs@icloud.com>
2022-09-06 18:31:21 -0300

    ci: add test coverage support

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+const config = {
+  preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testMatch: ['<rootDir>/src/**/*'],
+};
+
+module.exports = config;
